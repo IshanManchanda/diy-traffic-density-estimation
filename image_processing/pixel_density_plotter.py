9f0ecd7378dbe5a15982d7b44eadaadd0573fc14
anjali-2504 <75211465+anjali-2504@users.noreply.github.com>
2021-07-02 13:55:08 +0530

    Update pixel_density_plotter.py

--- image_processing/pixel_density_plotter.py
+++ image_processing/pixel_density_plotter.py
@@ -3,11 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import matplotlib
+base_path = os.path.dirname(os.path.abspath(__file__))
+img_path = os.path.join(
+	base_path, "empty.jpg"
+)
+video_path = os.path.join(base_path, "trafficvideo.mp4")
 
 
-input_img=cv2.imread("/home/anjali/Downloads/empty.jpg")
+input_img=cv2.imread("img_path")
 
-cap= cv2.VideoCapture('/home/anjali/Downloads/trafficvideo.mp4')
+cap= cv2.VideoCapture('video_path')
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 
